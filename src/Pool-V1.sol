b49d6594c500cc6efdd999ada605adc3f6544270
0xdavinchee <0xdavinchee@gmail.com>
2023-08-01 14:51:22 +0300

    remove last unused import

--- src/Pool-V1.sol
+++ src/Pool-V1.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
